a1f0f1ac79213fcc8ae4b21216da6bb61d818377
danil <minakonn9shkoni@gmail.com>
2022-05-24 14:48:54 +0300

    Create counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+
 
 function App() {
+  const [counter,setCounter]=useState(0);
+
+  const actionIncrement=()=>{
+    setCounter(s=>s+1);
+  }
+  const actionDecrement=()=>{
+    setCounter(s=>s-1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Counter:{counter}
+      <div>
+        <button onClick={actionIncrement}>+</button>
+        <button onClick={actionDecrement}>-</button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
